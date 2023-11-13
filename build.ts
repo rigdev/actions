f100c400544694d3241e4e1d84c8e6ecb2f9d6fd
Matias Frank Jensen <matias@rig.dev>
2023-11-13 13:19:45 +0100

    Update actions to comply with rig API changes

--- build.ts
+++ build.ts
@@ -1,27 +1,61 @@
-import { getInput, setOutput, setFailed } from "@actions/core";
-import { makeClient } from "./lib.js";
+import { getInput, setFailed, setOutput } from "@actions/core";
+import { getBool, makeClient, parseDeployOutput } from "./lib.js";
 
 interface Inputs {
+  project: string;
   image: string;
   capsule: string;
   skipImageCheck: boolean;
+  deploy: boolean;
+  environment: string;
+  dryRun: boolean;
+  force: boolean;
 }
 
 async function action(inputs: Inputs) {
+  if (inputs.deploy && inputs.environment == "") {
+    throw new Error("deploy was true, but environment not given.");
+  }
   const client = await makeClient();
-  let response = await client.build.createBuild({
+  let response = await client.build.create({
     skipImageCheck: inputs.skipImageCheck,
     image: inputs.image,
     capsuleId: inputs.capsule,
+    projectId: inputs.project,
   });
   setOutput("build", response.buildId);
+
+  if (inputs.deploy) {
+    let resp = await client.capsule.deploy({
+      projectId: inputs.project,
+      environmentId: inputs.environment,
+      capsuleId: inputs.capsule,
+      dryRun: inputs.dryRun,
+      force: inputs.force,
+      changes: [
+        {
+          field: {
+            case: "buildId",
+            value: response.buildId,
+          },
+        },
+      ],
+    });
+    let output = parseDeployOutput(resp.resourceYaml);
+    setOutput("rolloutConfig", output);
+  }
 }
 
 try {
   action({
+    project: getInput("project"),
     image: getInput("image"),
     capsule: getInput("capsule"),
-    skipImageCheck: getInput("skipImageCheck") == "true" ? true : false,
+    environment: getInput("environment"),
+    skipImageCheck: getInput("skipImageCheck") === "true" ? true : false,
+    deploy: getBool("deploy"),
+    dryRun: getBool("dryRun"),
+    force: getBool("force"),
   });
 } catch (e: any) {
   setFailed(e.message);
--- deploy.ts
+++ deploy.ts
@@ -1,15 +1,23 @@
-import { getInput, setFailed } from "@actions/core";
-import { makeClient } from "./lib.js";
+import { getInput, setFailed, setOutput } from "@actions/core";
+import { getBool, makeClient, parseDeployOutput } from "./lib.js";
 
 interface Inputs {
+  project: string;
+  environment: string;
   capsule: string;
   build: string;
+  dryRun: boolean;
+  force: boolean;
 }
 
 async function action(inputs: Inputs) {
   const client = await makeClient();
-  await client.capsule.deploy({
+  let resp = await client.capsule.deploy({
+    projectId: inputs.project,
+    environmentId: inputs.environment,
     capsuleId: inputs.capsule,
+    dryRun: inputs.dryRun,
+    force: inputs.force,
     changes: [
       {
         field: {
@@ -19,12 +27,18 @@ async function action(inputs: Inputs) {
       },
     ],
   });
+  let output = parseDeployOutput(resp.resourceYaml);
+  setOutput("rolloutConfig", output);
 }
 
 try {
   action({
+    project: getInput("project"),
+    environment: getInput("environment"),
     capsule: getInput("capsule"),
     build: getInput("build"),
+    dryRun: getBool("dryRun"),
+    force: getBool("force"),
   });
 } catch (e: any) {
   setFailed(e.message);
